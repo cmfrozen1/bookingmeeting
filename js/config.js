c72bb55838846b728768cadf181fb71fbdeb2ad6
cmfrozen1 <aatkcmf@gmail.com>
2026-03-11 15:46:51 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// js/config.js
+const CONFIG = {
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbwEkTSINEvnyBdapUWbJ8djVpdLt6rN7NlaXn5cKMow7RJtY3gI19-RSMyFHsWBxII-vA/exec',
+    LIFF_ID: '2009198981-GsqxkwIK',
+    CACHE_TTL: 5 * 60 * 1000,
+    MAX_RETRY: 3
+};
